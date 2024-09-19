c039b09bd9b143977689843b5f04583d48753ede
Eugenia Sansierra <eugesans@gmail.com>
2024-09-19 20:31:18 -0300

    agregamos la lista de rutas

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,23 @@ import "./App.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import Administrador from "./components/pages/Administrador.jsx";
 import FormularioProducto from "./components/pages/FormularioProducto.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Error404 from "./components/pages/Error404.jsx";
 
 
 function App() {
-  
+ 
 
   return (
-    <>
-      <Menu></Menu>
-      {/* <Index></Index> */}
-            <Administrador></Administrador>
-      {/* <FormularioProducto></FormularioProducto> */}
-      <Footer></Footer>
-          </>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element = { <Index></Index>}></Route>
+      <Route path="/Administrador" element = { <Administrador></Administrador>}></Route>
+      <Route path="/administrador/crear" element = { <FormularioProducto></FormularioProducto>}></Route>
+      <Route path="/administrador/editar" element = { <FormularioProducto></FormularioProducto>}></Route>
+      <Route path="*" element={<Error404></Error404>}></Route>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/pages/Error404.jsx
@@ -0,0 +1,12 @@
+import { Button } from "react-bootstrap";
+
+const Error404 = () => {
+    return (
+        <section className="text-center mt-3">
+            <h1>Error 404</h1>
+                <Button variant='warning'>Volver al inicio</Button>
+    </section>
+    );
+};
+
+export default Error404;
