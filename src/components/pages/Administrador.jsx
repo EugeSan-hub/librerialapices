8377aa9f1f2e422dc6327e56ab2709aaf64c1cf5
Eugenia Sansierra <eugesans@gmail.com>
2024-09-22 22:20:03 -0300

    limpiamos el formulario y mostramos cartel cuando se da de alta el prod o cuando falla

--- src/components/pages/Administrador.jsx
+++ src/components/pages/Administrador.jsx
@@ -1,18 +1,43 @@
 import { Button, Table } from "react-bootstrap";
 import ItemProducto from "./ItemProducto";
-import { useEffect } from "react";
-import { URLProductos } from "../../helpers/queries";
+import { useEffect, useState } from "react";
+import { leerProductosAPI, URLProductos } from "../../helpers/queries";
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const Administrador = () => {
+  const [productos, setProductos] = useState([]);
+
   useEffect(() => {
-    console.log(URLProductos);
+    obtenerProductos();
   }, []);
+
+  const obtenerProductos = async () => {
+    try {
+      const respuesta = await leerProductosAPI(); // Call to API function
+      if (respuesta.status === 200) {
+        const datos = await respuesta.json();
+        setProductos(datos);
+      } else {
+        Swal.fire({
+          title: "Ocurrió un error",
+          text: "No se pudo obtener el listado de productos, intente nuevamente más tarde",
+          icon: "error",
+        });
+      }
+    } catch (error) {
+      Swal.fire({
+        title: "Error",
+        text: "No se pudo conectar con el servidor",
+        icon: "error",
+      });
+    }
+  };
   return (
     <section className="container mainSection">
       <div className="d-flex justify-content-between align-items-center mt-5">
         <h1 className="display-4 ">Productos disponibles</h1>
-        <Link className='btn btn-primary'  to={'/administrador/crear'}>
+        <Link className="btn btn-primary" to={"/administrador/crear"}>
           <i className="bi bi-file-earmark-plus"></i>
         </Link>
       </div>
